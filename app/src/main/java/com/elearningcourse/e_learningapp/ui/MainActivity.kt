2586bfd039845bb8ba531f761f7abf53082fd2c1
Chris Vera <chrissmith132@gmail.com>
2022-09-18 19:00:38 +0300

    added support for Up navigation, changed labels for Units

--- app/src/main/java/com/elearningcourse/e_learningapp/ui/MainActivity.kt
+++ app/src/main/java/com/elearningcourse/e_learningapp/ui/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.elearningcourse.e_learningapp.R
@@ -15,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home_learn_material,
                 R.id.navigation_questions_answers,
@@ -53,5 +56,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Handle Up navigation from Action Bar
+     * */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 
 }
